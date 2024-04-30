425c5bc1e300293ac1146f66bc4515d53ba9d209
leilaab <sahel_70@yahoo.com>
2024-04-30 22:51:45 +0330

    first commit

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,51 @@
+import React from "react"
+import "./style.css"
+import { Col, Container, Row } from "react-bootstrap"
+
+const Footer = () => {
+  return (
+    <footer>
+        <Container>
+          <Row className="footer-row">
+            <Col md={3} sm={5} className='box'>
+              <div className="logo">
+                  <ion-icon name="bag"></ion-icon>
+                  <h1>Easy Shopping</h1>
+              </div>
+              <p>best shop less pay </p>
+            </Col>
+            <Col md={3} sm={5} className='box'>
+              <h2>About Us</h2>
+              <ul>
+                <li>Careers</li>
+                <li>Our Stores</li>
+                <li>Our Cares</li>
+              
+               
+              </ul>
+            </Col>
+            <Col md={3} sm={5} className='box'>
+              <h2>Customer Care</h2>
+              <ul>
+                <li>Help Center </li>
+                <li>How to Buy </li>
+                <li>Track Your Order </li>
+               
+                <li>Returns & Refunds </li>
+              </ul>
+            </Col>
+            <Col md={3} sm={5} className='box'>
+              <h2>Contact Us</h2>
+              <ul>
+                <li>Tehran Iran </li>
+                <li>Email:help@gmail.com</li>
+                <li>Phone: 0217000000</li>
+              </ul>
+            </Col>
+          </Row>
+        </Container>
+    </footer>
+  )
+}
+
+export default Footer
